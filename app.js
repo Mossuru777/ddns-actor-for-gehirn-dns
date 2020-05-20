011452db68768548fdc3b0fb824007d5aeb41dea
Mossan <mossuru777@gmail.com>
2020-05-20 13:26:48 +0900

    IPをキャッシュできるようにした

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const {HTTPBinIPGetter, RS500KIIPGetter} = require("./lib/ip-getter");
 const c = require("ansi-colors");
 const config = require("./config");
 
+const ip_getter = (() => {
+    if (config.ip_getter.type === "HTTPBin") {
+        return new HTTPBinIPGetter(config.ip_getter.ip_cache_path);
+    } else if (config.ip_getter.type === "RS-500KI") {
+        return new RS500KIIPGetter(config.ip_getter.ip_cache_path, config.ip_getter.options);
+    } else {
+        error_process(`Invalid Config: ip_getter.type (Unknown Type) -> ${config.ip_getter.type}`);
+    }
+})();
 const dns = new GehirnDNS(config.auth.token, config.auth.secret, config.option);
 
 const error_process = function (text) {
@@ -10,26 +19,47 @@ const error_process = function (text) {
     process.exit(1);
 };
 
+const caching_ip = async ip => {
+    process.stdout.write('Caching IP address ... ');
+    return ip_getter.setIpToCache(ip)
+      .then(() => {
+          console.log(c.bold.green('Done'));
+      })
+      .catch(reason => {
+          console.log(c.bold.red(`Failed: ${reason}`));
+      });
+};
+
 const update_dns_record = async function () {
     console.log(c.bold.underline('DDNS actor for GehirnDNS'));
 
     process.stdout.write('Checking global ip address ... ');
-    const ip_getter = (() => {
-        if (config.ip_getter.type === "HTTPBin") {
-            return new HTTPBinIPGetter();
-        } else if (config.ip_getter.type === "RS-500KI") {
-            return new RS500KIIPGetter(config.ip_getter.options);
-        } else {
-            error_process(`[Unknown IP Getter Type] ${config.ip_getter.type}`);
-        }
-    })();
     let current_ip;
     try {
         // タイムアウト5秒で取得を行う
         current_ip = await ip_getter.getGlobalIP(5000);
         console.log(c.bold.green('Done'));
     } catch (err) {
-        error_process(`[Global IP Get Error] ${err}`);
+        error_process(`Error: ${err}`);
+    }
+
+    process.stdout.write('Checking cache ip address ... ');
+    const cached_ip = await ip_getter.getIpFromCache()
+      .then(ip => {
+          console.log(c.bold.green('Done'));
+          return ip;
+      })
+      .catch(reason => {
+          console.log(c.yellowBright(`Skip: ${reason}`));
+          return null;
+      });
+
+    if (cached_ip !== null) {
+        console.log(`CACHE: ${cached_ip} <=> CURRENT: ${current_ip}`);
+        if (cached_ip === current_ip) {
+            console.log(c.bold.cyan('Cache is latest IP address.'));
+            process.exit(0);
+        }
     }
 
     process.stdout.write('Checking DNS zones ... ');
@@ -60,9 +90,12 @@ const update_dns_record = async function () {
     })();
     console.log(c.bold.green('Done'));
 
-    console.log(`GHRN: ${target_record.records[0].address} <=> GLOB: ${current_ip}`);
+    console.log(`GEHIRN: ${target_record.records[0].address} <=> CURRENT: ${current_ip}`);
     if (target_record.records[0].address === current_ip) {
-        console.log(c.bold.cyan('Record is set latest IPaddress.'));
+        console.log(c.bold.cyan('Record is set latest IP address.'));
+        if (target_record.records[0].address !== cached_ip) {
+            await caching_ip(target_record.records[0].address);
+        }
         process.exit(0);
     }
 
@@ -87,6 +120,9 @@ const update_dns_record = async function () {
     }
 
     console.log(c.bold.green('Complete!'));
+    if (target_record.records[0].address !== cached_ip) {
+        await caching_ip(target_record.records[0].address);
+    }
     process.exit(0);
 };
 
